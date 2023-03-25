4413e5d61022691faa2129166b43df8ea437ddf3
Кирилл <120108672+KirillCH1@users.noreply.github.com>
2023-03-25 12:59:20 +0300

    Create Task26.py

---
+++ Task26.py
@@ -0,0 +1,16 @@
+#  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+
+def DegreeNumber(a,i) :
+    if i == 1:
+        return a
+    return a * DegreeNumber(a,i-1)
+
+try:
+    a = int(input('Введите число:'))
+    b = int(input('Введите степень числа:'))
+    i =int(0)
+    print(f'\"{a}\" в степени \"{b}\" равно {DegreeNumber(a,b)}')
+    
+except:
+    print('')
